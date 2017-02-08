801d351cc6d1d10d947c563b3c2979ffe9f32d25
SupervisionT <a.alshaikh82@gmail.com>
2017-02-08 00:37:31 -0800

    update for day 2

--- src.js
+++ src.js
@@ -1,27 +1,26 @@
+var i = 5;
+
 function timer(){
 
 	var min = [1,5,10];
 	timemilliseconds = min.map(function(x){
 		return x*60*1000;
 	})
-	
-	var i = 5;
-	var obj = {
-		onTime: function(){
-			i--;
-			if(i < 0){
-				console.log("times up!!");
-			}else{
-				console.log("i= ",i);
-				setTimeout(obj.onTime, 1000);
-			}
-		//return obj
-		},
-		_time:i
-
+		var y = onTime();
+		console.log(y);
+		return y
 	}
 
+	function onTime(){
+		if(i < 0){
+			console.log("times up!! i = ",i+1);
+			return i;
+		}else{
+			console.log("i= ",i);
+			i = i-1;
+			setTimeout(onTime, 1000);
+		}
+	//return i;
+	}
 	//var i = 10;
-	//setTimeout(function(){console.log(i);}, 5000); 
-	return obj;
-}
+	//setTimeout(function(){console.log(i);}, 5000);
--- test.js
+++ test.js
@@ -4,14 +4,28 @@ test("Check if function timer exists",function(t){
   t.ok(window.timer,"it exists")
 });
 
-test("Check if funtion timer returns time in milliseconds",function(t){
+/*test("Check if funtion timer returns time in milliseconds",function(t){
  min = [1,5,10];
  t.deepEqual(timer(),[60000, 300000,600000],"it works")
 });
 
+/*
 test("Check if funtion timer is working",function(t){
  var a = timer();
  a.onTime();
- QUnit.stop(5);
- t.deepEqual(a._time,5,"it works")
+ //QUnit.stop(5);
+ t.deepEqual(a._time,0,"it works")
+});
+*/
+
+Test('my test', function(t) {
+    //var a = timer();
+    //a.onTime;
+    console.log(a);
+    var done = t.async();
+    setTimeout(function() {
+        s//tart(); // this would tell QUnit to start the test
+        t.deepEqual(a,0,"it works");
+        done();
+    }, 6000);
 });
